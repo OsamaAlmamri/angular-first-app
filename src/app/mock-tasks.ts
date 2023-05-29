38ea240c20871f67827052286719c64a88d518be
Osama Al-mamari <yemencoder@gmail.com>
2023-05-29 17:57:16 +0300

    add mock  tasks data

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./task"
+
+export const TASKS:Task[]=[
+    {
+        id:1,
+        text:"task 1 ", 
+        day:"May 5th at 2:30pm",
+        remender:true
+    },
+    {
+        id:2,
+        text:"task 2", 
+        day:"May 6th at 12:30pm",
+        remender:true
+    },
+    {
+        id:3,
+        text:"task 3 ", 
+        day:"May 8th at 3:30pm",
+        remender:true
+    },
+    {
+        id:4,
+        text:"task4 ", 
+        day:"Jun 8th at 4:30pm",
+        remender:true
+    }
+]
---
+++ src/app/task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    id? : Number,
+    text:String, 
+    day:String,
+    remender:boolean
+}
